91ccaf80025dd84b404691bc6edad71f45c42921
Harlod Bombala, Cody Boelens <h.bombala@student.avans.nl>
2025-03-10 14:27:52 +0100

    Initial Commit

---
+++ MyBookEye/MyBookEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookEyeApp.swift
+//  MyBookEye
+//
+//  Created by Harlod Bombala on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyBookEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBookEyeTests/MyBookEyeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyBookEyeTests.swift
+//  MyBookEyeTests
+//
+//  Created by Harlod Bombala on 10/03/2025.
+//
+
+import Testing
+
+struct MyBookEyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
